0be7e82e94ac85db4da14260747339529c919cf9
Lucas <us3.lucas@gmail.com>
2025-05-07 23:12:11 -0300

    Update employee-class.java

--- Exercicio/employee-class.java
+++ Exercicio/employee-class.java
@@ -1,6 +1,6 @@
 public class Employee {
 
-   private Integer id;
+    private Integer id;
     private String name;
     private Double salary;
 
@@ -33,11 +33,13 @@ public class Employee {
     public void increaseSalary(double percentage) {
         salary += salary * percentage / 100.0;
     }
+    @Override
     public String toString() {
         return id + ", " + name + ", " + String.format("%.2f", salary);
     }
     public void add(Employee emp) {
 
+      
     }
 }
 
